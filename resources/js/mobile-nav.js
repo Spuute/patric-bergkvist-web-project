94d80ab3fea85c12f405309f93e33fd6f538a5f5
Patric Bergkvist <70053524+Spuute@users.noreply.github.com>
2021-02-15 21:38:54 +0100

    Added stuff
    Added the grid.css to the project.
    Started to build the top navbar.
    Added a separate js file for the mobile-navigation menu.

---
+++ resources/js/mobile-nav.js
@@ -0,0 +1,8 @@
+(function navigationSlide() {
+  const responsiveMenu = document.querySelector(".mobile-menu");
+  const navigation = document.querySelector(".main-navigation");
+
+  responsiveMenu.addEventListener("click", () => {
+    navigation.classList.toggle("mobile-nav-active");
+  });
+})();
